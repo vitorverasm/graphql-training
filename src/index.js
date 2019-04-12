1bde3fe7b430ca6c640a1b132d0d966240fb5508
Vitor Veras <vitor.veras@secrel.net.br>
2019-04-12 18:49:37 -0300

    Add some Link queries

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 };
 
