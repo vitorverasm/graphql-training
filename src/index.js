8e41f5733030622b1290f16f0938c4848e2155f0
Vitor Veras <vitorverasm@gmail.com>
2019-04-23 22:22:50 -0300

    Implement CRUD operations

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer } = require("graphql-yoga");
 
 let links = [
   {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  },
+  {
+    id: "link-1",
+    url: "www.teste1.com",
+    description: "Teste 1"
+  },
+  {
+    id: "link-2",
+    url: "www.teste2.com",
+    description: "Teste 2"
   }
 ];
 let idCount = links.length;
@@ -14,33 +24,55 @@ const resolvers = {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
     link: (parent, args) => {
-      return links.filter(item => {
-        if (item.id == args.id) {
-          return item;
+      const { id } = args;
+      return links.find(post => {
+        if (post.id === id) {
+          return true;
         }
-      })
+      });
     }
-  },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
   },
   Mutation: {
     post: (parent, args) => {
       const link = {
-        id: `link-${idCount}`,
+        id: `link-${idCount++}`,
         description: args.description,
         url: args.url
       };
       links.push(link);
       return link;
+    },
+    updateLink: (parent, args) => {
+      const { id, url, description } = args;
+      let postIndex = links.findIndex(post => {
+        if (post.id === id) {
+          return true;
+        }
+      });
+      if (postIndex !== -1) {
+        links[postIndex].url = url;
+        links[postIndex].description = description;
+      }
+      return links[postIndex];
+    },
+    deleteLink: (parent, args) => {
+      const { id } = args;
+      let deletedPost;
+      let postIndex = links.findIndex(post => {
+        if (post.id === id) {
+          return true;
+        }
+      });
+      if (postIndex !== -1) {
+        deletedPost = links.splice(postIndex, 1).pop();
+      }
+      return deletedPost;
     }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers
 });
 server.start(() => console.log(`Server is running on http://localhost:4000`));
