ea62d74c42872caff95bf3ab454a8adfb8474e04
Vitor Veras <vitor.veras@secrel.net.br>
2019-05-02 18:52:59 -0300

    Add subscription functionalities

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link
 };
